a7af9d089f92da777b7a853945186dee6d465647
Robert Gessler <ragessler@Roberts-MacBook-Pro.local>
2022-05-09 11:39:22 -0500

    deleted some starter code + wrote code for DisplayMusic Component + created async function for getting all songs

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import DisplayMusic from './Components/DisplayMusic/DisplayMusic'
 import './App.css';
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+  
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs(){
+    let response  = await axios.get('http://127.0.0.1:8000/api/music/');
+    setSongs(response.data)
+    console.log(songs)
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <DisplayMusic parentSongs = {songs}/>
+      </div>
     </div>
   );
 }
